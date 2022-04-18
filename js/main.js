6ba138fc5b167400d842b08af09e39f09c727c53
saehee15 <sae_hwi@naver.com>
2022-04-18 17:27:48 +0900

    20220418_슬라이드 변경중

--- js/main.js
+++ js/main.js
@@ -17,11 +17,13 @@ $(function(){
     })
 
     // 탭
-     $('.tab a').click(function (e) {
+     $('.cont h3').click(function (e) {
         e.preventDefault();
 
-        idVal = $(this).data('tap')
-        $(this).addClass('on').siblings().removeClass('on')
+        var parent = $(this).parent()
+
+        // idVal = $(this).data('tab')
+        $(parent).addClass('active').siblings().removeClass('active')
 
         $('.'+idVal).addClass('on').siblings().removeClass('on')
     })
@@ -45,74 +47,137 @@ $(function(){
       }
     });
     
-    $(".pause").on("click", function () {
+    $(".pause1").on("click", function () {
       $(".slide1-1").slick("slickPause");
     });
     
-    $(".play").on("click", function () {
+    $(".play1").on("click", function () {
       $(".slide1-1").slick("slickPlay");
     });
     
 
+    $(".slide1-2").slick({
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      speed: 2000,
+      autoplay: true,
+      arrows: true,
+      prevArrow: '<button type="button" class="slick-prev"></button>',
+      nextArrow: '<button type="button" class="slick-next"></button>',
+      dots: true,
+      dotsClass: "custom_paging",
+      customPaging: function (slider, i) {
+        console.log(slider);
+        return i + 1 + "/" + slider.slideCount;
+      }
+    });
+    
+    $(".pause2").on("click", function () {
+      $(".slide1-2").slick("slickPause");
+      $('.pause2').removeClass('on');
+      $('.play2').addClass('on');
+    });
+    
+    $(".play2").on("click", function () {
+      $(".slide1-2").slick("slickPlay");
+      $('.pause2').addClass('on');
+      $('.play2').removeClass('on');
+    });
+    
 
-    var content2 = new Swiper(".content2", {
-        loop: true,
-        autoplay: {
-            delay: 2500,
-            disableOnInteraction: false,
-          },
 
-        pagination: {
-          el: ".pages",
-          type: "fraction",
-        },
-        navigation: {
-            nextEl: ".next",
-            prevEl: ".prev",
-        },
-      });
+    // var content2 = new Swiper(".content2", {
+    //     loop: true,
+    //     autoplay: {
+    //         delay: 2500,
+    //         disableOnInteraction: false,
+    //       },
 
-      $('.btn_state2').click(function(){
-        if($('.btn_state2 .pause').hasClass('on')){
-            $('.btn_state2 .pause').removeClass('on');
-            $('.btn_state2 .play').addClass('on');
-            content2.autoplay.stop();
-        }else{
-            $('.btn_state2 .pause').addClass('on');
-            $('.btn_state2 .play').removeClass('on');
-            content2.autoplay.start();
-        }
-    })
+    //     pagination: {
+    //       el: ".pages",
+    //       type: "fraction",
+    //     },
+    //     navigation: {
+    //         nextEl: ".next",
+    //         prevEl: ".prev",
+    //     },
+    //   });
 
-    var slide2 = new Swiper(".slide2", {
-        slidesPerView: 'auto',
-        loop: true,
-        autoplay: {
-            delay: 2500,
-            disableOnInteraction: false,
-          },
+    //   $('.btn_state2').click(function(){
+    //     if($('.btn_state2 .pause').hasClass('on')){
+    //         $('.btn_state2 .pause').removeClass('on');
+    //         $('.btn_state2 .play').addClass('on');
+    //         content2.autoplay.stop();
+    //     }else{
+    //         $('.btn_state2 .pause').addClass('on');
+    //         $('.btn_state2 .play').removeClass('on');
+    //         content2.autoplay.start();
+    //     }
+    // })
 
-          pagination: {
-            el: ".pagination",
-            type: "fraction",
-          },
-        navigation: {
-          nextEl: ".next",
-          prevEl: ".prev",
-        },
-      });
+    // var slide2 = new Swiper(".slide2", {
+    //     slidesPerView: 'auto',
+    //     loop: true,
+    //     autoplay: {
+    //         delay: 2500,
+    //         disableOnInteraction: false,
+    //       },
 
-      $('.btn_state').click(function(){
-        if($('.btn_state .pause').hasClass('on')){
-            $('.btn_state .pause').removeClass('on');
-            $('.btn_state .play').addClass('on');
-            slide2.autoplay.stop();
-        }else{
-            $('.btn_state .pause').addClass('on');
-            $('.btn_state .play').removeClass('on');
-            slide2.autoplay.start();
-        }
-    })
+    //       pagination: {
+    //         el: ".pagination",
+    //         type: "fraction",
+    //       },
+    //     navigation: {
+    //       nextEl: ".next",
+    //       prevEl: ".prev",
+    //     },
+    //   });
+
+    //   $('.btn_state').click(function(){
+    //     if($('.btn_state .pause').hasClass('on')){
+    //         $('.btn_state .pause').removeClass('on');
+    //         $('.btn_state .play').addClass('on');
+    //         slide2.autoplay.stop();
+    //     }else{
+    //         $('.btn_state .pause').addClass('on');
+    //         $('.btn_state .play').removeClass('on');
+    //         slide2.autoplay.start();
+    //     }
+    // })
+
+    $(".slide2").slick({
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      speed: 2000,
+      autoplay: true,
+      arrows: true,
+      prevArrow: '<button type="button" class="slick-prev"></button>',
+      nextArrow: '<button type="button" class="slick-next"></button>',
+      dots: true,
+      dotsClass: "custom_paging",
+      customPaging: function (slider, i) {
+        console.log(slider);
+        return i + 1 + "/" + slider.slideCount;
+      }
+    });
+    
+    $(".pause2").on("click", function () {
+      $(".slide1-2").slick("slickPause");
+      $('.pause2').removeClass('on');
+      $('.play2').addClass('on');
+    });
+    
+    $(".play2").on("click", function () {
+      $(".slide1-2").slick("slickPlay");
+      $('.pause2').addClass('on');
+      $('.play2').removeClass('on');
+    });
 
     // 서브메뉴
     $(document).on("click", ".section9 li", function () {
